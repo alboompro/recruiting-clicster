a294de6bd75a0444c47218f34bf36bc67ef821c4
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-12-09 13:39:26 -0200

    Starting the backend project

---
+++ back-end/src/config/config.php
@@ -0,0 +1,8 @@
+<?php
+$config['displayErrorDetails'] = true;
+$config['addContentLengthHeader'] = false;
+
+$config['db']['host']   = "localhost";
+$config['db']['user']   = "geovane";
+$config['db']['pass']   = "";
+$config['db']['dbname'] = "exampleapp";
