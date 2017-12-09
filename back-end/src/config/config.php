009c80ca5ae8055a7777e2724ee72ac89129c744
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-12-09 15:49:47 -0200

    Setting CORS authorization and renaming dbname

--- back-end/src/config/config.php
+++ back-end/src/config/config.php
@@ -5,4 +5,4 @@ $config['addContentLengthHeader'] = false;
 $config['db']['host']   = "localhost";
 $config['db']['user']   = "geovane";
 $config['db']['pass']   = "";
-$config['db']['dbname'] = "exampleapp";
+$config['db']['dbname'] = "alboom";
