f4e7504cdb14030f14fcabbc6f1b8cc10879cee2
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-12-10 18:07:02 -0200

    Automatic client list created

--- front-end/js/controllers/clients-controller.js
+++ front-end/js/controllers/clients-controller.js
@@ -1,13 +1,46 @@
-angular.module('agenda').controller('ClientsController', function($scope, clientResource) {
-
+angular.module('agenda').controller('ClientsController', [ '$scope', 'clientResource', function($scope, clientResource) {
+	
 	$scope.clients = [];
 	$scope.filter = '';
 	$scope.message = '';
 
-	clientResource.query(function(clients) {
-		$scope.clients = clients;
-	}, function(erro) {
-		console.log(erro);
-	});
+	// clientResource.query(function(clients) {
+	// 	$scope.clients = clients;
+	// }, function(erro) {
+	// 	console.log(erro);
+	// });
 
-});
+	$scope.clients = [
+		{
+			name: 'Geovane Pacheco da Rocha 1',
+			company: 'Alboom',
+			image: 'client.png'
+		},
+		{
+			name: 'Geovane Pacheco da Rocha 2',
+			company: 'Alboom',
+			image: 'client.png'
+		},
+		{
+			name: 'Geovane Pacheco da Rocha 3',
+			company: 'Alboom',
+			image: 'client.png'
+		},
+		{
+			name: 'Geovane Pacheco da Rocha 4',
+			company: 'Alboom',
+			image: 'client.png'
+		},
+		{
+			name: 'Geovane Pacheco da Rocha 5',
+			company: 'Alboom',
+			image: 'client.png'
+		},
+		{
+			name: 'Geovane Pacheco da Rocha 6',
+			company: 'Alboom',
+			image: 'client.png'
+		}
+	]
+
+}]);
--- front-end/js/main.js
+++ front-end/js/main.js
@@ -1,11 +1,12 @@
-angular.module('agenda', ['ngAnimate', 'ngRoute', 'ngResource'])
+angular.module('agenda', ['directives', 'services','ngAnimate', 'ngRoute', 'ngResource'])
 	.config(function($routeProvider, $locationProvider) {
 
 		$locationProvider.html5Mode(true);
 
 		$routeProvider.when('/', {
 			templateUrl: 'partials/main.html',
-			controller: 'AgendaController'
+			// controller: 'AgendaController'
+			controller: 'ClientsController'
 		});
 
 		$routeProvider.otherwise({redirectTo: '/'});
