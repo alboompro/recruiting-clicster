bd4f643597d735a5bc8f2a1abb2e59af2e4c7f2d
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-12-10 22:39:17 -0200

    Setting the configuration to use eloquent ORM

--- back-end/src/app/start.php
+++ back-end/src/app/start.php
@@ -7,11 +7,36 @@ spl_autoload_register(function ($classname) {
 
     if( file_exists( $currentDirectory . "/app/controller/" . $classname . ".php" ) )
         require ( $currentDirectory . "/app/controller/" . $classname . ".php" );
+
+    if( file_exists( $currentDirectory . "/app/model/" . $classname . ".php" ) )
+        require ( $currentDirectory . "/app/model/" . $classname . ".php" );
 });
 
+$config['determineRouteBeforeAppMiddleware'] = false;
 $config['displayErrorDetails'] = true;
 
+$config['db']['driver']     = 'mysql';
+$config['db']['host']       = 'localhost';
+$config['db']['database']   = 'alboom';
+$config['db']['username']   = 'geovane';
+$config['db']['password']   = '';
+$config['db']['charset']    = 'utf8';
+$config['db']['collation']  = 'utf8_general_ci';
+$config['db']['prefix']     = '';
+
 $app = new \Slim\App(['settings' => $config]);
+
+$container = $app->getContainer();
+
+$container['db'] = function ($container) {
+    $capsule = new \Illuminate\Database\Capsule\Manager;
+    $capsule->addConnection($container['settings']['db']);
+
+    $capsule->setAsGlobal();
+    $capsule->bootEloquent();
+
+    return $capsule;
+};
 
 require 'routes.php';
 
