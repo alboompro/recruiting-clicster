3c84b3f758866452cbeb0d31484ca8df6835bba9
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-12-11 18:46:09 -0200

    Applying the search client functionality

--- front-end/js/controllers/clients-controller.js
+++ front-end/js/controllers/clients-controller.js
@@ -1,13 +1,13 @@
 angular.module('agenda').controller('ClientsController', [ '$scope', '$http', 'clientResource', function($scope, $http, clientResource) {
 
 	$scope.clients = [];
-	$scope.filter = '';
+	$scope.clientFilter = '';
 	$scope.message = '';
 
 	clientResource.query(function(clients) {
 		$scope.clients = clients;
 	}, function(erro) {
 		console.log(erro);
-	});	
+	});
 
 }]);
--- front-end/js/main.js
+++ front-end/js/main.js
@@ -1,6 +1,6 @@
 angular.module('agenda', ['directives', 'services','ngAnimate', 'ngRoute', 'ngResource'])
 	.config(function($routeProvider, $locationProvider) {
-    
+
 		$locationProvider.html5Mode(true);
 
 		$routeProvider.when('/clients', {
