f4fe3014a669f4431523336c21bb34c3e2810a65
Geovane Pacheco <geovane.pacheco99@gmail.com>
2017-12-10 22:41:51 -0200

    Controllers configured to use eloquent ORM and the model layer

--- back-end/src/app/controller/BaseController.php
+++ back-end/src/app/controller/BaseController.php
@@ -4,16 +4,16 @@ use Interop\Container\ContainerInterface;
 
 abstract class BaseController
 {
+    protected $db;
+
     public function __construct(ContainerInterface $ci)
     {
-
+        $this->db = $ci->get('db');
     }
 
     public function __invoke($request, $response, $args)
     {
 
     }
-
-
 
 }
--- back-end/src/app/controller/ClientController.php
+++ back-end/src/app/controller/ClientController.php
@@ -6,12 +6,13 @@ class ClientController extends BaseController
 {
     public function __construct(ContainerInterface $ci)
     {
-
+        Parent::__construct($ci);
     }
 
     public function index($request, $response, $args)
     {
-        return $response;
+        $clientModel = new ClientModel($this->db);
+        return $response->withJson($clientModel->get());
     }
-    
+
 }
